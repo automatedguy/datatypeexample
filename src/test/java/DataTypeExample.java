a1014202f1f76b7c76552e0d7949fade1fdcb4fc
Gabriel Cespedes <gcespedes@overactiveinc.com>
2019-12-26 17:35:12 -0300

    Added ArrayList

--- src/test/java/DataTypeExample.java
+++ src/test/java/DataTypeExample.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class DataTypeExample {
 
     /**
@@ -7,9 +9,8 @@ public class DataTypeExample {
     public static void main(String[] args) {
 
         Car carOne = new Car(4, 2, 4, "B", false, "Honda", "ASDF3456");
-
         Car carTwo = new Car(4, 2, 4, "B", false, "Honda", "ASDF3456");
-
+        Car carNnn = new Car(4, 2, 4, "B", false, "Honda", "ASDF3456");
 
         carOne.setDoors(2);
         carOne.setFullDomain("DFDGFDGFDFD");
@@ -19,5 +20,10 @@ public class DataTypeExample {
         carOne.getDoors();
         carOne.getCHASIS_NUM();
 
+        ArrayList<Car> carArrayList = new ArrayList<Car>();
+
+        carArrayList.add(carOne);
+        carArrayList.add(carTwo);
+        carArrayList.add(carNnn);
     }
 }
